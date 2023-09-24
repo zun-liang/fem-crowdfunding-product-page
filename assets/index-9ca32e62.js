1e272d0cc38741c48513712028dc23497987ef5f
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-09-25 05:52:44 +0800

    Updates

--- assets/index-9ca32e62.js
+++ assets/index-9ca32e62.js
@@ -552,7 +552,6 @@ Error generating stack: `+l.message+`
     body {
         width: 100%;
         height: 100%;
-        min-height: 100vh;
         font-family: 'Commissioner', sans-serif;
         background-color: hsl(0, 0%, 97%);
         overflow-x: hidden;
